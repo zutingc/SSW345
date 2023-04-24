fd96081aa9ede59a273216a93c33ae980cee4c20
zchen125 <zchen125@stevens.edu>
2023-04-24 10:03:15 -0400

    SSW345

---
+++ Visitor/ComputerPart.java
@@ -0,0 +1,6 @@
+package Visitor;
+
+public interface ComputerPart {
+    public void accept(ComputerPartVisitor computerPartVisitor);
+    }
+    
---
+++ Visitor/VisitorPatternDemo.java
@@ -0,0 +1,8 @@
+package Visitor;
+
+public class VisitorPatternDemo {
+    public static void main(String[] args) {
+        ComputerPart computer = new Computer();
+        computer.accept(new ComputerPartDisplayVisitor());
+    }
+}
